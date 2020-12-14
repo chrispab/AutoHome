b63da42dbab9355719e9d99795045c26d7086573
chrispab <cbattisson@gmail.com>
2020-12-14 23:19:18 +0000

    xmas vlights

--- automation/jsr223/python/personal/christmasLights.py
+++ automation/jsr223/python/personal/christmasLights.py
@@ -8,17 +8,22 @@ from core.actions import ScriptExecution
 # def heating_startup(event):
 
 
+# @rule("Turn ON xmas lights via proxy", description="Handles xmas actions", tags=["xmas"])
+# @when("Item ChristmasLightsProxy changed from OFF to ON")
+# def xmas_lights_on(event):
+#     xmas_lights_on.log.error("Turn ON the xmas lights via proxy")
+#     events.sendCommand("gChristmasWifiPowerSockets", "ON")
+
+
 @rule("Turn ON xmas lights via proxy", description="Handles xmas actions", tags=["xmas"])
-# @when("Item CT_Temperature changed")
-@when("Item ChristmasLightsProxy changed from OFF to ON")
+@when("Item ChristmasLightsProxy received update ON")
 def xmas_lights_on(event):
-    xmas_lights_on.log.error("Turn ON the xmas lights via proxy")
+    xmas_lights_on.log.error("Turn ON the xmas lights via  proxy")
     events.sendCommand("gChristmasWifiPowerSockets", "ON")
-    # events.sendCommand("workLightsPowerSocket", "ON")
 
 
 @rule("Turn OFF conservatory lights via proxy", description="Handles fan actions", tags=["conservatory", "fan"])
-@when("Item ChristmasLightsProxy changed from ON to OFF")
+@when("Item ChristmasLightsProxy received update OFF")
 def xmas_lights__off(event):
     xmas_lights__off.log.error("Turn Off the xmas lights via proxy")
     events.sendCommand("gChristmasWifiPowerSockets", "OFF")
