8e9d0731eaa29bd2a20a52a98692634e07196d96
chrispab <cbattisson@gmail.com>
2021-01-23 13:33:44 +0000

    socket 7

--- automation/jsr223/python/personal/pir.py
+++ automation/jsr223/python/personal/pir.py
@@ -16,9 +16,13 @@ reload(personal.util)
 @when("Item pir01_occupancy received update")
 @when("Item pir02_occupancy received update")
 def pir_change(event):
-  #  pir_change.log.info("/////=======pir_change trigger item : " + event.itemName + ", PREV: " + PersistenceExtensions.previousState(ir.getItem(event.itemName), True).state + ", NOW: " + event.itemState.toString())
-    pir_change.log.error("///./..............pir_light received update")
+   pir_change.log.error("pir_occupancy received update item : " + event.itemName + ", PREV: " + "PersistenceExtensions.previousState(ir.getItem(event.itemName), True)" + ", NOW: " + event.itemState.toString())
+    # pir_change.log.error("pir01__occupancy received update")
     # send_info("test", pir_change.log)                                                                                                #PersistenceExtensions.previousState(ir.getItem("Weather_SolarRadiation"), True).state
+
+lights_timeout = 120
+pir01_off_timer = None
+pir02_off_timer = None
 
 
 @rule("pir01 or 02 Turn ON lights", description="PIRsensor change", tags=["pir"])
@@ -28,13 +32,25 @@ def pir_light_on(event):
     pir_light_on.log.error("pir_occupancy received update. item : " + event.itemName + ": " +
                           event.itemState.toString() + ": " + items["pir01_illuminance_lux"].toString())
 
+    global pir01_off_timer
+    global pir02_off_timer
+
     if items["BridgeLightSensorLevel"] < items["ConservatoryLightTriggerLevel"] :
         if items["pir01_occupancy"] == ON:
             events.sendCommand("KT_light_1_Power", "ON")
+            if pir01_off_timer is not None and not pir01_off_timer.hasTerminated():
+                pir01_off_timer.cancel()
+                pir_light_on.log.error("CANCEL     STOP running pir01_off_timer")
+
         if items["pir02_occupancy"] == ON:
             events.sendCommand("KT_light_2_Power", "ON")
             events.sendCommand("KT_light_3_Power", "ON")
+            if pir02_off_timer is not None and not pir02_off_timer.hasTerminated():
+                pir02_off_timer.cancel()
+                pir_light_on.log.error("CANCEL     STOP running pir02_off_timer")
+        # ! also stop any currently running lighht off timers
 
+# chargerTimer2 is not None and not chargerTimer2.hasTerminated():
 
 @rule("PIRsensor Turn OFF lights", description="PIRsensor Turn OFF lights", tags=["pir"])
 @when("Item pir01_occupancy changed from ON to OFF") # or
@@ -42,13 +58,16 @@ def pir_light_on(event):
 def pir_light_off(event):
     pir_light_off.log.error("///// pir_occupancy_off triggering item : " + event.itemName + " : " + event.itemState.toString())
 
+    global pir01_off_timer
+    global pir02_off_timer
+
     if event.itemName == "pir01_occupancy":
-        # pir_light_off.log.error("pir01_occupancy_off body : KT_light_1_Power: OFF ")
-        pir01_off_timer = ScriptExecution.createTimer(DateTime.now().plusSeconds(30), lambda: pir01_off_body())
+        pir_light_off.log.error("pir01_occupancy: STARTING TIMER KT_light_1_Power: OFF ")
+        pir01_off_timer = ScriptExecution.createTimer(DateTime.now().plusSeconds(lights_timeout), lambda: pir01_off_body())
 
     if event.itemName == "pir02_occupancy":
-        # pir_light_off.log.error("pir02_occupancy_off body : KT_light_2_Power: OFF ")
-        pir02_off_timer = ScriptExecution.createTimer(DateTime.now().plusSeconds(30), lambda: pir02_off_body())
+        pir_light_off.log.error("pir02_occupancy : STARTING TIMER  KT_light_2&3_Power: OFF ")
+        pir02_off_timer = ScriptExecution.createTimer(DateTime.now().plusSeconds(lights_timeout), lambda: pir02_off_body())
 
 def pir_off_body():
     if items["pir01_occupancy"] == OFF and items["pir02_occupancy"] == OFF:
