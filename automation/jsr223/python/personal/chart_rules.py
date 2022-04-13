e0c3e87364ca0a5d557624ee563f9e9bf2662429
chrispab <cbattisson@gmail.com>
2022-04-13 11:02:06 +0100

    chart refresh

--- automation/jsr223/python/personal/chart_rules.py
+++ automation/jsr223/python/personal/chart_rules.py
@@ -2,12 +2,12 @@
 # var ZonedDateTime = Java.type("java.time.ZonedDateTime");
 # var now = ZonedDateTime.now();
 
-from core.rules import rule
-from core.triggers import when
-from core.actions import LogAction
-from core.actions import ScriptExecution
-from java.time import ZonedDateTime as DateTime
+# from core.rules import rule
+# from core.triggers import when
+# from core.actions import LogAction
+# from core.actions import ScriptExecution
+# from java.time import ZonedDateTime as DateTime
 
-# runtime.events.sendCommand('UI_Refresh_Timer', now.toLocalDateTime().toString());
+# # runtime.events.sendCommand('UI_Refresh_Timer', now.toLocalDateTime().toString());
 
-events.sendCommand('UI_Refresh_Timer', DateTime.now().toString())
+# events.sendCommand('UI_Refresh_Timer', DateTime.now().toString())
