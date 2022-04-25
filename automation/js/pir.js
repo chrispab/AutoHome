340019940621cf4a9c26800f2f8e8fe8c426c982
chrispab <cbattisson@gmail.com>
2022-04-25 12:50:28 +0100

    pre 3.0.0.0M3 release

--- automation/js/pir.js
+++ automation/js/pir.js
@@ -1,31 +1,19 @@
-// # # from personal.util import send_info
-// # from core.rules import rule
-// # from core.triggers import when
-// # from core.actions import PersistenceExtensions
-// # from core.actions import ScriptExecution
-// # from core.actions import LogAction
-// # from java.time import ZonedDateTime as DateTime
-
 // # import personal.util
 // # reload(personal.util)
 
-// # @rule("PIRsensor change", description="PIRsensor change", tags=["pir"])
-// # @when("Item pir01_occupancy received update")
-// # @when("Item pir02_occupancy received update")
-// # def pir_change(event):
-// #    pir_change.log.debug("*********pir_occupancy received update item : " + event.itemName + ", PREV: " + "PersistenceExtensions.previousState(ir.getItem(event.itemName), True)" + ", NOW: " + event.itemState.toString())
 rules.JSRule({
   name: "PIR sensors change",
   description: "PIR sensors change",
   triggers: [triggers.ItemStateUpdateTrigger("pir01_occupancy"), triggers.ItemStateUpdateTrigger("pir02_occupancy")],
   execute: (data) => {
-    // items.getItem("ZbWhiteBulb01Switch").sendCommand("OFF");
-    // items.getItem("ZbWhiteBulb02Switch").sendCommand("OFF");
-    // actions.NotificationAction.sendNotification(email, "Balcony lights are  OFF");
-    console.error("!!!!!!!!!!!!  pir_occupancy received update itemName : " + data.itemName + ", state: " +items.getItem(data.itemName).state + ", PREV state: " + items.getItem(data.itemName).history.previousState());
-    // console.error("*********************************  conservatory_fan_cool_recirc_off - - CT_Fan433PowerSocket OFF");
-    // items.getItem("CT_Fan433PowerSocket").sendCommand("OFF");
-
+    console.error(
+      "!!!!!!!!!!!!  pir_occupancy received update itemName : " +
+        data.itemName +
+        ", state: " +
+        items.getItem(data.itemName).state +
+        ", PREV state: " +
+        items.getItem(data.itemName).history.previousState()
+    );
   },
 });
 
@@ -57,7 +45,41 @@ rules.JSRule({
 // #             if pir02_off_timer is not None and not pir02_off_timer.hasTerminated():
 // #                 pir02_off_timer.cancel()
 // #                 pir_light_on.log.debug("CANCEL     STOP running pir02_off_timer")
-// #         # ! also stop any currently running lighht off timers
+rules.JSRule({
+  name: "pir01 or 02 updated to ON - Turn ON lights  ",
+  description: "pir01 or 02 Turn ON lights",
+  triggers: [
+    triggers.ItemStateUpdateTrigger("pir01_occupancy", "ON"),
+    triggers.ItemStateUpdateTrigger("pir02_occupancy", "ON"),
+  ],
+  execute: (data) => {
+    console.error(
+      "ON ON ON =======  pir_occupancy received update itemName : " +
+        data.itemName +
+        ", state: " +
+        items.getItem(data.itemName).state +
+        ", PREV state: " +
+        items.getItem(data.itemName).history.previousState()
+    );
+
+    // #     if items["BridgeLightSensorLevel"] < items["ConservatoryLightTriggerLevel"] :
+    if (items.getItem("BridgeLightSensorLevel").state < items.getItem("ConservatoryLightTriggerLevel").state) {
+      // #         if items["pir01_occupancy"] == ON:
+
+      // #             events.sendCommand("KT_light_1_Power", "ON")
+      // #             pir_light_on.log.debug("rxed pir01_occupancy ON ")
+      // #             if pir01_off_timer is not None and not pir01_off_timer.hasTerminated():
+      // #                 pir01_off_timer.cancel()
+      // #                 pir_light_on.log.debug("CANCEL     STOP running pir01_off_timer")
+      // #         if items["pir02_occupancy"] == ON:
+      // #             events.sendCommand("KT_light_2_Power", "ON")
+      // #             events.sendCommand("KT_light_3_Power", "ON")
+      // #             if pir02_off_timer is not None and not pir02_off_timer.hasTerminated():
+      // #                 pir02_off_timer.cancel()
+      // #                 pir_light_on.log.debug("CANCEL     STOP running pir02_off_timer")
+    }
+  },
+});
 
 // # # chargerTimer2 is not None and not chargerTimer2.hasTerminated():
 
