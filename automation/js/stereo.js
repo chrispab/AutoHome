1ed9cf7926a4f0b666102bc2129fa6fb013088ab
chrispab <cbattisson@gmail.com>
2022-05-21 20:59:19 +0100

    tv redo

--- automation/js/stereo.js
+++ automation/js/stereo.js
@@ -6,9 +6,9 @@ rules.JSRule({
     triggers.ItemStateChangeTrigger("vCT_stereo", "OFF", "ON"),
     ],
     execute: (data) => {
-        console.error("Turning on stereo - kodi, amp,and selecting Aux" );
-        items.getItem("bg_wifisocket_3_1_power").sendCommand("ON");//amp ir bridge hdmi audio extractor
-        items.getItem("bg_wifisocket_1_1_power").sendCommand("ON");//kodi pi
+        console.error("STEREO Turning on stereo - kodi, amp" );
+        items.getItem("bg_wifisocket_1_1_power").sendCommand("ON");//kodi, amp, ir bridge, hdmi audio extractor
+        // items.getItem("bg_wifisocket_1_1_power").sendCommand("ON");//kodi pi
 
         //if there is a request to turn off the stereo in progress cancel it as we want it on!
         if (CT_stereo_off_timer && CT_stereo_off_timer.isActive()) {
@@ -17,13 +17,13 @@ rules.JSRule({
 
         //     t_ampStandbyON = ScriptExecution.createTimer(DateTime.now().plusSeconds(45), lambda: events.sendCommand("amplifierPowerOn", "ON"))
         actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusSeconds(20), function () {
-            items.getItem("amplifierPowerOn").sendCommand("ON");// IR code
+            items.getItem("amplifier_IR_PowerOn").sendCommand("ON");// IR code
             console.error("STEREO - IR turn on amp from standby");
         });
         //     t_ampVideo01 = ScriptExecution.createTimer(DateTime.now().plusSeconds(60), lambda: events.sendCommand("amplifierVideo1", "ON"))
-        actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusSeconds(40), function () {
-            items.getItem("amplifierAux").sendCommand("ON");//IR code
-            console.error("STEREO - IR amp switch to aux source");
+        actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusSeconds(30), function () {
+            items.getItem("amplifier_IR_Aux").sendCommand("ON");//IR code
+            console.error("STEREO - IR amp switch to AUX source");
         });
     },
 });
@@ -39,13 +39,17 @@ rules.JSRule({
     ],
     execute: (data) => {
         console.error("Turning OFF stereo - kodi, amp, and bridges");
-        items.getItem("KodiConservatory_systemcommand").sendCommand("Shutdown");//shutdown CT Pi
-        items.getItem("bg_wifisocket_3_1_power").sendCommand("OFF");//amp ir bridge hdmi audio extractor
+        items.getItem("Kodi_CT_SendSystemCommand").sendCommand("Shutdown");//shutdown CT Pi
+        // console.error("err 2");
+
+        // items.getItem("bg_wifisocket_3_1_power").sendCommand("OFF");//amp ir bridge hdmi audio extractor
         console.error("STEREO - turned OFF amp, and bridges");
         //if stereo off timer is not defined or completed, restart the stereo off timer
         if (!CT_stereo_off_timer || !CT_stereo_off_timer.isActive()) {
             CT_stereo_off_timer = actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusSeconds(60), function () {
-                items.getItem("bg_wifisocket_1_1_power").sendCommand("OFF");//CT kodi pi
+                items.getItem("bg_wifisocket_1_1_power").sendCommand("OFF");//CT kodi, amp, ir bridge, hdmi audio extractor
+                items.getItem("vCT_stereo").postUpdate("OFF");//turn off virt trigger
+
                 console.error("STEREO - turned OFF kodi");
             });
         }
--- automation/jsr223/python/personal/tv_rules.py
+++ automation/jsr223/python/personal/tv_rules.py
@@ -71,15 +71,15 @@ def conservatory_tv_on(event):
     conservatory_tv_on.log.info("conservatory_tv_on")
     Voice.say("Turning on conservatory TV", "voicerss:enGB", "chromecast:chromecast:GHM_Conservatory", PercentType(50))
 
-    events.sendCommand("CT_TV_Power", "ON") #tv
-    events.sendCommand("bg_wifisocket_3_1_power", "ON") #amp ir bridge hdmi audio extractor
-    events.sendCommand("bg_wifisocket_1_1_power", "ON") # kodi pi
+    # events.sendCommand("CT_TV_Power", "ON") #tv
+    events.sendCommand("bg_wifisocket_1_2_power", "ON") #tv
+    events.sendCommand("bg_wifisocket_1_1_power", "ON") # kodi pi,amp ir bridge hdmi audio extractor
 
     if t_CTtvPowerOff is not None:
         t_CTtvPowerOff = None
 
-    t_ampStandbyON = ScriptExecution.createTimer(DateTime.now().plusSeconds(45), lambda: events.sendCommand("amplifierPowerOn", "ON"))
-    t_ampVideo01 = ScriptExecution.createTimer(DateTime.now().plusSeconds(60), lambda: events.sendCommand("amplifierVideo1", "ON"))
+    t_ampStandbyON = ScriptExecution.createTimer(DateTime.now().plusSeconds(45), lambda: events.sendCommand("amplifier_IR_PowerOn", "ON"))
+    t_ampVideo01 = ScriptExecution.createTimer(DateTime.now().plusSeconds(60), lambda: events.sendCommand("amplifier_IR_Video1", "ON"))
 
 
 #------------------------------------
@@ -94,17 +94,19 @@ def conservatory_tv_off(event):
 
     # events.postUpdate("shutdownKodiConservatoryProxy", "OFF") - this routine can be removed if this works
     LogAction.logError("Shutdown Conservatory Kodi","Shutdown Conservatory Kodi: {}", event.itemName)
-    events.sendCommand("KodiConservatory_systemcommand","Shutdown")
-    events.sendCommand("amplifierPowerOff", "ON")
+    events.sendCommand("Kodi_CT_SendSystemCommand","Shutdown")
+    events.sendCommand("amplifier_IR_PowerOff", "ON")
+    events.sendCommand("bg_wifisocket_1_2_power", "OFF") #tv
+
 
     if t_CTtvPowerOff is None:
-        t_CTtvPowerOff = ScriptExecution.createTimer(DateTime.now().plusSeconds(30), lambda: tvoffbody())
+        t_CTtvPowerOff = ScriptExecution.createTimer(DateTime.now().plusSeconds(60), lambda: tvoffbody())
 
 def tvoffbody():
     global t_CTtvPowerOff
-    events.sendCommand("CT_TV_Power", "OFF")
-    events.sendCommand("bg_wifisocket_3_1_power", "OFF") #amp ir bridge hdmi audio extractor
-    events.sendCommand("bg_wifisocket_1_1_power", "OFF") # kodi pi
+    # events.sendCommand("CT_TV_Power", "OFF")
+    # events.sendCommand("bg_wifisocket_1_2_power", "OFF") #tv
+    events.sendCommand("bg_wifisocket_1_1_power", "OFF") # kodi pi,amp ir bridge hdmi audio extractor
     t_CTtvPowerOff = None
 
 
