9b4c8b38f3bc1d309c0d26af88b0966e07a6b7f3
chrispab <cbattisson@gmail.com>
2022-09-26 23:04:38 +0100

    boiler

--- automation/js/BG_Availability.js
+++ automation/js/BG_Availability.js
@@ -1,21 +1,3 @@
-// from core.rules import rule
-// from core.triggers import when
-// from core.actions import LogAction
-// from core.actions import ScriptExecution
-// from java.time import ZonedDateTime as DateTime
-
-// # https://community.openhab.org/t/design-pattern-motion-sensor-timer/14954
-// timers = {}
-// timeoutSeconds = 31  # use an appropriate value
-
-// @rule("init BG avail status", description="zb temp sensors init", tags=["heating"])
-// @when("System started")
-// def init_BG_status(event):
-//     init_BG_status.log.debug("init_BG_status")
-//     for item in ir.getItem("gBG_sockets_reachable").members:
-//         events.postUpdate(item, "Offline")
-
-// const { time, items } = require('openhab-js');
 const {
   log, items, rules, actions,
 } = require('openhab');
@@ -27,7 +9,7 @@ scriptLoaded = function () {
   // const { items } = require('@runtime');#
   console.log(items);
   logger.warn('scriptLoaded -   init   BG avail statusesss');
-  myutils.showGroupMembers('gBG_sockets_reachable');
+  // myutils.showGroupMembers('gBG_sockets_reachable');
 
   items.getItem('gBG_sockets_reachable').members.forEach((item) => {
     // whatitis = `${whatitis + batt.label}: ${batt.state}\r\n`;
@@ -37,23 +19,6 @@ scriptLoaded = function () {
   myutils.showGroupMembers('gBG_sockets_reachable');
 };
 
-// # when a BG socket MQTT 'maxworktime' update comes in from device - updates frequency determined by broadlink2mqtt
-// # use maxworktime as the value to monitor as using a pwr state may also come from rule updates etc and be a false presence
-// @rule("update BG sockets Online/Offline status", description="monitor BG MQTT updates", tags=["BG"])
-// @when("Member of gBG_socket_maxworktime_updates received update")
-// def bgAvail(event):
-//    ## # LogAction.logDebug("gBG_socket_maxworktime_updates", "!!!! gBG_socket_maxworktime_updates  Item {} received  update: {}", event.itemName, event.itemState)
-
-//     # bgAvail.log.debug("!!!!bbb gBG_socket_maxworktime_updates  Item " + event.itemName + "received  update: " + event.itemState.toString())
-//     # create the 'reachable' item name e.g bg_wifisocket_4_maxworktime to bg_wifisocket_4_reachable
-//     item_name_reachable = event.itemName[:event.itemName.rfind('_')+1] + "reachable"
-//     events.postUpdate(item_name_reachable, "Online")  # use reachable not triggering event cos its temp
-//     # bgAvail.log.debug("== BG sockets Online/Offline status marked  ONLINE::")
-
-//     if event.itemName not in timers or timers[event.itemName].hasTerminated():
-//         timers[event.itemName] = ScriptExecution.createTimer(DateTime.now().plusSeconds(timeoutSeconds), lambda: events.postUpdate(item_name_reachable, "Offline"))
-//     else:
-//         timers[event.itemName].reschedule(DateTime.now().plusSeconds(timeoutSeconds))
 const timers = {};// [];
 const timeoutSeconds = 60; // use an appropriate value
 const { timeUtils } = require('openhab_rules_tools');
@@ -64,7 +29,7 @@ rules.JSRule({
   triggers: [triggers.GroupStateUpdateTrigger('gBG_socket_maxworktime_updates')],
   execute: (event) => {
     console.warn(`update BG sockets Online/Offline status, triggering item name: ${event.itemName} : ,  received  update event.receivedState: ${event.receivedState}`);
-    myutils.showGroupMembers('gBG_socket_maxworktime_updates');
+    // myutils.showGroupMembers('gBG_socket_maxworktime_updates');
 
     const stub = event.itemName.toString().substr(0, event.itemName.lastIndexOf('_'));
     const itemNameReachable = `${stub}_reachable`;
