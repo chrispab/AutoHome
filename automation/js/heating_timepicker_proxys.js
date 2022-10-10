352521b0ab5050ff9a82f9ab74516299fd1020d7
chrispab <cbattisson@gmail.com>
2022-10-10 22:55:15 +0100

    clean

--- automation/js/heating_timepicker_proxys.js
+++ automation/js/heating_timepicker_proxys.js
@@ -6,6 +6,30 @@ const { myutils } = require('personal');
 const logger = log('timepicker.js');
 const { timeUtils } = require('openhab_rules_tools');
 
+// function turnOnTV(onOffProxyItem, powerControlItem, message) {
+//   logger.warn(`Turning on Pi Kodi and TV:${message}`);
+//   actions.Voice.say(message);
+
+//   // if off timer defined (someone tried to turn tv off), stop it so it dosent prevent powering ON
+//   if (!(tvPowerOffTimer === undefined)) {
+//     tvPowerOffTimer.cancel();// = undefined;
+//   }
+//   items.getItem(onOffProxyItem).postUpdate('ON');
+//   items.getItem(powerControlItem).sendCommand('ON');
+// }
+
+rules.JSRule({
+  name: 'If vFanHeater_Enable timeline trigger',
+  description: 'If the vFanHeater_Enable timeline sends an event-respond',
+  triggers: [triggers.ItemStateUpdateTrigger('vFanHeater_Enable')],
+  execute: (data) => {
+    logger.warn('__>>timeline trigger');
+    logger.warn(`__>>data triggger item : ${items.getItem(data.itemName).name}`);
+    logger.warn(`__>>data triggger item state: ${items.getItem(data.itemName).state}`);
+    items.getItem('fan_heater_enable').sendCommand(items.getItem(data.itemName).state);
+  },
+});
+
 rules.JSRule({
   name: 'If any timepicker',
   description: 'If any heaters demanding heat, turn Boiler ON else turn boiler OFF',
