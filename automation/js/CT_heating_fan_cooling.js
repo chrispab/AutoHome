6969e8c8a0d4ab68dbbc4ee71156d5e8c33d0ce4
chrispab <cbattisson@gmail.com>
2022-11-28 23:15:58 +0000

    fan

--- automation/js/CT_heating_fan_cooling.js
+++ automation/js/CT_heating_fan_cooling.js
@@ -1,57 +1,16 @@
 const {
-  log, items, rules, actions, triggers,
+  log, items, rules, actions, triggers, time,
 } = require('openhab');
 // const { myutils } = require('personal');
 
-const logger = log('fan.js');
+const logger = log('CT_heating_fan_cooling.js');
 // const { timeUtils } = require('openhab_rules_tools');
 // const { toToday } = require('openhab_rules_tools/timeUtils');
 
 scriptLoaded = function () {
-  logger.warn('scriptLoaded fan.js');
+  logger.warn('scriptLoaded fCT_heating_fan_cooling.js');
   // let loadedDate = Date.now();
 };
-
-rules.JSRule({
-  name: 'conservatory fan circulate heat Cron',
-  description: 'conservatory fan circulate heat Cron',
-  triggers: [triggers.GenericCronTrigger('0 0/5 * * * ?')],
-  execute: () => {
-    logger.debug('ZZZZZ  conservatory fan circulate heat Cron   ZZZZZ');
-    const fanOnSecs = 240;
-    const setPoint = items.getItem('CT_TemperatureSetpoint').state;
-    const temp = items.getItem('CT_Temperature').state;
-    if (setPoint >= 18 && temp < setPoint && items.getItem('CT_Fan_Heating_circulate_enable').state == 'ON') {
-      logger.debug('ZZZZZ  conservatory fan circulate heat rulel turn FAN ON NOW   ZZZZZ');
-      items.getItem('CT_Fan433PowerSocket').sendCommand('ON');
-
-      actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusSeconds(fanOnSecs), () => {
-        items.getItem('CT_Fan433PowerSocket').sendCommand('OFF');
-        // items.getItem("FanPulseSwitch").sendCommand("OFF");
-        logger.debug('ZZZZZ  conservatory_fan recic heat turn FAN OFF NOW   ');
-      });
-    }
-  },
-});
-
-rules.JSRule({
-  name: 'Fan heat recirc ENABLE turned ON',
-  description: 'Fan heat recirc ENABLE turned ON',
-  triggers: [triggers.ItemStateChangeTrigger('CT_Fan_Heating_circulate_enable', 'OFF', 'ON')],
-  execute: (data) => {
-    logger.debug('conservatory_fan_heat_recirc_on - no action required');
-  },
-});
-
-rules.JSRule({
-  name: 'Fan heat recirc ENABLE turned off',
-  description: 'Fan heat recirc ENABLE turned off',
-  triggers: [triggers.ItemStateChangeTrigger('CT_Fan_Heating_circulate_enable', 'ON', 'OFF')],
-  execute: (data) => {
-    logger.debug('conservatory_fan_heat_recirc_off - CT_Fan433PowerSocket OFF');
-    items.getItem('CT_Fan433PowerSocket').sendCommand('OFF');
-  },
-});
 
 rules.JSRule({
   name: 'conservatory fan_cool rule',
@@ -69,7 +28,7 @@ rules.JSRule({
     // alerting.sendInfo('FROM fan OPENHABvv');
     // alerting.sendAlert('The following Chromecast devices are now in use');
 
-    if (items.getItem('CT_Fan_Cooling_enable').state == 'ON') {
+    if (items.getItem('CT_Fan_Cooling_enable').state.toString() === 'ON') {
       logger.debug('conservatory_fan_ cool rulel - detected CT_Fan_Cooling_enable   ON');
       const setPoint = items.getItem('Conservatory_Fan_ON_Setpoint').state;
       const temp = items.getItem('CT_Temperature').state;
@@ -111,8 +70,7 @@ rules.JSRule({
   description: 'Fan Pulse (FanPulseSwitch) change/update',
   triggers: [triggers.ItemStateChangeTrigger('FanPulseSwitch', 'OFF', 'ON')],
   execute: (data) => {
-    logger.debug('fan pulse CT_Fan433PowerSocket ON');
-
+    logger.error('PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPp fan pulse CT_Fan433PowerSocket ON');
     items.getItem('CT_Fan433PowerSocket').sendCommand('ON');
     actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusSeconds(10), () => {
       items.getItem('CT_Fan433PowerSocket').sendCommand('OFF');
---
+++ automation/js/CT_heating_fan_recirculate.js
@@ -0,0 +1,53 @@
+const {
+  log, items, rules, actions, triggers, time,
+} = require('openhab');
+// const { myutils } = require('personal');
+
+const logger = log('CT_heating_fan_recirculate.js');
+// const { timeUtils } = require('openhab_rules_tools');
+// const { toToday } = require('openhab_rules_tools/timeUtils');
+
+scriptLoaded = function () {
+  logger.warn('scriptLoaded fan.js');
+};
+
+rules.JSRule({
+  name: 'conservatory fan circulate heat Cron',
+  description: 'conservatory fan circulate heat Cron',
+  triggers: [triggers.GenericCronTrigger('0 0/5 * * * ?')],
+  execute: () => {
+    logger.debug('ZZZZZ  conservatory fan circulate heat Cron   ZZZZZ');
+    const fanOnSecs = 60;
+    const setPoint = items.getItem('CT_TemperatureSetpoint').state;
+    const temp = items.getItem('CT_Temperature').state;
+    if (setPoint >= 18 && temp < setPoint && items.getItem('CT_Fan_Heating_circulate_enable').state.toString() === 'ON') {
+      logger.error('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFfff  conservatory fan circulate heat rulel turn FAN ON NOW   ZZZZZ');
+      items.getItem('CT_Fan433PowerSocket').sendCommand('ON');
+
+      actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusSeconds(fanOnSecs), () => {
+        items.getItem('CT_Fan433PowerSocket').sendCommand('OFF');
+        // items.getItem("FanPulseSwitch").sendCommand("OFF");
+        logger.error('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFfff  conservatory_fan recic heat turn FAN OFF NOW   ');
+      });
+    }
+  },
+});
+
+rules.JSRule({
+  name: 'Fan heat recirc ENABLE turned ON',
+  description: 'Fan heat recirc ENABLE turned ON',
+  triggers: [triggers.ItemStateChangeTrigger('CT_Fan_Heating_circulate_enable', 'OFF', 'ON')],
+  execute: (data) => {
+    logger.debug('conservatory_fan_heat_recirc_on - no action required');
+  },
+});
+
+rules.JSRule({
+  name: 'Fan heat recirc ENABLE turned off',
+  description: 'Fan heat recirc ENABLE turned off',
+  triggers: [triggers.ItemStateChangeTrigger('CT_Fan_Heating_circulate_enable', 'ON', 'OFF')],
+  execute: (data) => {
+    logger.debug('conservatory_fan_heat_recirc_off - CT_Fan433PowerSocket OFF');
+    items.getItem('CT_Fan433PowerSocket').sendCommand('OFF');
+  },
+});
