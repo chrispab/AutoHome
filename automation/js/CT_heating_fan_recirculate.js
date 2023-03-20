40be7bfaf6f333d562c1257f8ee0d8f314ac5268
chrispab <cbattisson@gmail.com>
2023-03-20 22:25:18 +0000

    kn rtv

--- automation/js/CT_heating_fan_recirculate.js
+++ automation/js/CT_heating_fan_recirculate.js
@@ -14,19 +14,22 @@ scriptLoaded = function () {
 rules.JSRule({
   name: 'conservatory fan circulate heat Cron',
   description: 'conservatory fan circulate heat Cron',
-  triggers: [triggers.GenericCronTrigger('0 0/4 * * * ?')],
+  triggers: [triggers.GenericCronTrigger('0 0/3 * * * ?')],
   execute: () => {
     logger.warn('ZZZZZ  conservatory fan circulate heat Cron   ZZZZZ');
-    const fanOnSecs = 15;
+    const fanOnSecs = 45;
     const setPoint = items.getItem('CT_TemperatureSetpoint').state;
     const temp = items.getItem('CT_Temperature').state;
     // and heater on
-    if (((setPoint >= 18 && temp < setPoint) || items.getItem('CT_Heater').state.toString() === 'ON') && items.getItem('CT_Fan_Heating_circulate_enable').state.toString() === 'ON') {
+    // if (((setPoint >= 18 && temp < setPoint) || items.getItem('CT_Heater').state.toString() === 'ON') && items.getItem('CT_Fan_Heating_circulate_enable').state.toString() === 'ON') {
+    if (((setPoint >= 18) || items.getItem('CT_Heater').state.toString() === 'ON') && items.getItem('CT_Fan_Heating_circulate_enable').state.toString() === 'ON') {
       // logger.error('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFfff  conservatory fan circulate heat rulel turn FAN ON NOW   ZZZZZ');
-      items.getItem('CT_fan_power').sendCommand('ON');
+      // items.getItem('CT_fan_power').sendCommand('ON');
+      items.getItem('wifi_socket_5_power').sendCommand('ON');
 
       actions.ScriptExecution.createTimer(time.ZonedDateTime.now().plusSeconds(fanOnSecs), () => {
-        items.getItem('CT_fan_power').sendCommand('OFF');
+        // items.getItem('CT_fan_power').sendCommand('OFF');
+        items.getItem('wifi_socket_5_power').sendCommand('OFF');
         // items.getItem("FanPulseSwitch").sendCommand("OFF");
         // logger.error('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFfff  conservatory_fan recic heat turn FAN OFF NOW   ');
       });
