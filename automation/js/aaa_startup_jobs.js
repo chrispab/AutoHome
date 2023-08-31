deb063c6d597cbdc5e92bdb6af842ee8107ec105
chrispab <cbattisson@gmail.com>
2023-08-31 17:19:50 +0100

    clean

--- automation/js/aaa_startup_jobs.js
+++ automation/js/aaa_startup_jobs.js
@@ -13,14 +13,15 @@ scriptLoaded = function () {
 };
 
 rules.JSRule({
-  name: 'SystemStartlevelTrigger(100)',
+  name: 'SystemStartlevelTrigger_100',
   description: 'SystemStartlevelTrigger(100)',
   // triggers: [triggers.SystemStartlevelTrigger(100)],
   triggers: [triggers.SystemStartlevelTrigger(100)],
 
   execute: () => {
+
     // actions.Audio.playSound('0600_hours_regeneration_cycle_complete_normalised.mp3');
-    actions.Audio.playSound('input_ok_3_clean.mp3');
+    actions.Audio.playSound('barking.mp3');
     alerting.sendInfo('triggers.SystemStartlevelTrigger(100)');
 
   },
