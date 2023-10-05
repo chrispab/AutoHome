ef6c31d7c6ff23e02b967dae45e414a5cfa7747e
chrispab <cbattisson@gmail.com>
2023-10-05 22:34:42 +0100

    sonds

--- automation/js/CT_kodi_status.js
+++ automation/js/CT_kodi_status.js
@@ -1,12 +1,18 @@
-// const thingStatusInfo = actions.Things.getThingStatusInfo("zwave:serial_zstick:512");
+const {
+  log, items, rules, actions, triggers,
+} = require('openhab');
+const { alerting } = require('personal');
+var ruleUID = "CT_kodi_status";
+
+const logger = log(ruleUID);// const thingStatusInfo = actions.Things.getThingStatusInfo("zwave:serial_zstick:512");
 // console.log("Thing status",thingStatusInfo.getStatus());
 // triggers.ThingStatusChangeTrigger('some:thing:uuid','ONLINE','OFFLINE')
 scriptLoaded = function () {
-  console.error('======set KodiConservatory status scriptLoaded');
+  logger.info('======set KodiConservatory status scriptLoaded');
   // loadedDate = Date.now();
-  console.error('startup- set Kodi_CT_Online_Status status ');
+  logger.info('startup- set Kodi_CT_Online_Status status ');
   const thingStatusInfo = actions.Things.getThingStatusInfo('kodi:kodi:4cc97fc0-c074-917d-e452-aed8219168eb');
-  console.error('Thing Kodi_CT_Online_Status status', thingStatusInfo.getStatus());
+  logger.info('Thing Kodi_CT_Online_Status status', thingStatusInfo.getStatus());
 
   if (thingStatusInfo.getStatus().toString() == 'ONLINE') {
     items.getItem('Kodi_CT_Online_Status').postUpdate('ONLINE');
@@ -20,9 +26,9 @@ rules.JSRule({
   description: 'set Kodi_CT_Online_Status status',
   triggers: [triggers.ThingStatusChangeTrigger('kodi:kodi:4cc97fc0-c074-917d-e452-aed8219168eb')],
   execute: (data) => {
-    console.error('set Kodi_CT_Online_Status status');
+    logger.info('set Kodi_CT_Online_Status status');
     const thingStatusInfo = actions.Things.getThingStatusInfo('kodi:kodi:4cc97fc0-c074-917d-e452-aed8219168eb');
-    console.error('Thing Kodi_CT_Online_Status status', thingStatusInfo.getStatus());
+    logger.info('Thing Kodi_CT_Online_Status status', thingStatusInfo.getStatus());
 
     if (thingStatusInfo.getStatus().toString() == 'ONLINE') {
       items.getItem('Kodi_CT_Online_Status').postUpdate('ONLINE');
