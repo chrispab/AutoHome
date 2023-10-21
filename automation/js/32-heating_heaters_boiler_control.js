e3726eef0cf7541cdd10f193eb121b5972a8fe19
chrispab <cbattisson@gmail.com>
2023-10-21 16:53:30 +0100

    heating

--- automation/js/32-heating_heaters_boiler_control.js
+++ automation/js/32-heating_heaters_boiler_control.js
@@ -120,3 +120,40 @@ rules.JSRule({
     }
   },
 });
+
+
+rules.JSRule({
+  name: 'handle MasterHeatingMode updated',
+  description: 'handle MasterHeatingMode updated',
+  triggers: [
+    triggers.ItemStateUpdateTrigger('masterHeatingMode'),
+  ],
+  execute: () => {
+    logger.debug('handle MasterHeatingMode updated');
+    const masterHeatingModeItemState = items.getItem('masterHeatingMode').state.toString();
+
+    switch (masterHeatingModeItemState) {
+      case 'off':
+        // turn 'off' all individual room heating modes
+        logger.debug('make all rooms - off - MASTER Heating Mode :');
+        items.getItem('gHeatingModes').sendCommand('off');
+        // todo do not override mode if manual
+        break;
+      case 'manual':
+        logger.debug('make all rooms - manual - MASTER Heating Mode :');
+        // items.getItem('gHeatingModes').sendCommand('manual');
+        // todo do not override mode if manual
+        break;
+      case 'auto':
+        // check each heater heating mode
+        logger.debug('make all rooms - AUTO - MASTER Heating Mode :');
+        items.getItem('gHeatingModes').sendCommand('auto');
+        // todo do not override mode if manual
+        break;
+      default:
+        logger.debug(`Unkown master heating mode: ${masterHeatingModeItemState}`);
+    }
+    // events.sendCommand("Heating_UpdateHeaters", "ON") #trigger updating of heaters and boiler etc
+    items.getItem('Heating_UpdateHeaters').sendCommand('ON');
+  },
+});
