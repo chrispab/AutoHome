9119bb8c60979e272ce802a637d9aa64cb1e0a82
chrispab <cbattisson@gmail.com>
2023-10-22 11:19:11 +0100

    fh

--- automation/js/34-heating_fan_heater.js
+++ automation/js/34-heating_fan_heater.js
@@ -2,13 +2,13 @@ const {
   log, items, rules, actions, time, triggers,
 } = require('openhab');
 var ruleUID = "fan_heater";
-
 const logger = log(ruleUID);
+
 
 scriptLoaded = function () {
   logger.info('scriptLoaded - init ft ct sp link');
-  const offset = items.getItem('FH_Link_TrackingOffset').state;
-  if (offset === 'NULL') {
+  const tracking_offset = items.getItem('FH_Link_TrackingOffset').state;
+  if (tracking_offset === 'NULL') {
     items.getItem('FH_Link_TrackingOffset').sendCommand(1.0);
   }
 };
@@ -21,70 +21,74 @@ rules.JSRule({
     triggers.ItemStateChangeTrigger('CT_ThermostatTemperatureAmbient'),
     triggers.ItemStateChangeTrigger('FH_LinkSetpointToCTSetpoint'),
     triggers.ItemStateChangeTrigger('FH_Link_TrackingOffset'),
-
   ],
   execute: (data) => {
-    logger.debug('________Link fan heater setpoint to CT setpoint triggered');
+    logger.debug('Link fan heater setpoint to CT setpoint triggered');
     if (items.getItem('FH_LinkSetpointToCTSetpoint').state === 'ON') {
-      // track
-      logger.debug('________tracking to CT setpoint');
-      // get ct sp and copy to fh sp - offset
-      let CT_SP = items.getItem('CT_ThermostatTemperatureSetpoint').rawState;
-      const FH_ThermostatTemperatureSetpoint_item = items.getItem('FH_ThermostatTemperatureSetpoint');
+
+      logger.debug('tracking to CT setpoint');
+      // get ct setpoint and copy to fh setpoint - tracking_offset
+      var CT_ThermostatTemperatureSetpoint_raw = items.getItem('CT_ThermostatTemperatureSetpoint').rawState;
+      var FH_ThermostatTemperatureSetpoint_item = items.getItem('FH_ThermostatTemperatureSetpoint');
 
       // add offest
-      const offset = items.getItem('FH_Link_TrackingOffset').rawState;
-      CT_SP -= offset;
+      var tracking_offset = items.getItem('FH_Link_TrackingOffset').rawState;
+      var new_FH_ThermostatTemperatureSetpoint = CT_ThermostatTemperatureSetpoint_raw - tracking_offset;
       // copy/track
-      FH_ThermostatTemperatureSetpoint_item.sendCommand(CT_SP);
+      FH_ThermostatTemperatureSetpoint_item.sendCommand(new_FH_ThermostatTemperatureSetpoint);
     }
   },
 });
 
 rules.JSRule({
-  name: 'fan heater check',
+  name: 'check for fan heater demand',
   description: 'If fan heater demand turn on fan heater',
   triggers: [
-    // triggers.ItemStateChangeTrigger('fan_heater_temperature_sensor'),
     triggers.ItemStateChangeTrigger('FH_ThermostatTemperatureAmbient'),
-    // triggers.ItemStateChangeTrigger('CT_Temperature'),
     // triggers.ItemStateChangeTrigger('fan_heater_ON_Setpoint'),
-    // triggers.ItemStateChangeTrigger('FH_enable'),
     triggers.ItemStateChangeTrigger('FH_ThermostatTemperatureSetpoint'),
-    triggers.ItemStateChangeTrigger('FH_enable'),
+    triggers.ItemStateChangeTrigger('FH_Heater_Enable'),
     triggers.ItemStateChangeTrigger('FH_outside_temperature_enable'),
-
   ],
   execute: (data) => {
-    logger.debug('________If fan heater demand turn on fan heater');
+    logger.debug('If fan heater demand turn on fan heater');
 
-    if (items.getItem('FH_enable').state.toString() === 'OFF') {
-      logger.debug('________items.getItem(FH_enable).state == OFF');
-      items.getItem('fan_heater').sendCommand('OFF');
+    FH_Heater_Enable_Item_State =items.getItem('FH_Heater_Enable').state.toString();
+
+    //dont process further if fan heater disabled
+    if (FH_Heater_Enable_Item_State === 'OFF') {
+      logger.debug('FH_Heater_Enable_Item_State === {}', FH_Heater_Enable_Item_State);
+      items.getItem('FH_Heater_Control').sendCommand('OFF');
+      logger.debug('FH_Heater_Control.sendCommand(OFF), and exit from - If fan heater demand turn on fan heater');
       return;
     }
 
-    if (items.getItem('FH_enable').state.toString() === 'ON') {
-      // console.error('######################################################_____mvm___PAST THE GATE');
-      if ((items.getItem('Outside_Temperature').rawState <= items.getItem('FH_outside_temperature_enable').rawState)) {
-        const setPoint = items.getItem('FH_ThermostatTemperatureSetpoint').rawState;
-        logger.debug(`________FH_ThermostatTemperatureSetpoint: ${setPoint}`);
-        const temp = items.getItem('FH_ThermostatTemperatureAmbient').rawState;
+    //if fan heater enable = ON, process
+    if (FH_Heater_Enable_Item_State === 'ON') {
+      logger.debug('FH_Heater_Enable_Item_State === {}', FH_Heater_Enable_Item_State);
 
-        if (temp < setPoint) {
+      //if outside current_room_temp < fan heater outside temp enable - go
+      var Outside_Temperature_raw =items.getItem('Outside_Temperature').rawState;
+      var FH_outside_temperature_enable_raw= items.getItem('FH_outside_temperature_enable').rawState
+
+      if ((Outside_Temperature_raw <= FH_outside_temperature_enable_raw)) {
+        var FH_Thermostat_SetPoint = items.getItem('FH_ThermostatTemperatureSetpoint').rawState;
+        logger.debug('FH_ThermostatTemperatureSetpoint: {}', FH_Thermostat_SetPoint);
+        var current_room_temp = items.getItem('FH_ThermostatTemperatureAmbient').rawState;
+        logger.debug('current_room_temp: {},', FH_Thermostat_SetPoint);
+
+        if (current_room_temp < FH_Thermostat_SetPoint) {
           items.getItem('FH_Heater_Control').sendCommand('ON');
-          logger.debug('>>>>- temp < setPoint turning heater ON');
-        } else if (temp >= (setPoint)) { // (items.getItem('CT_Heater_Control').state == 'OFF') && (
+          logger.debug('current_room_temp < FH_Thermostat_SetPoint turning heater ON');
+        } else if (current_room_temp >= (FH_Thermostat_SetPoint)) {
           items.getItem('FH_Heater_Control').sendCommand('OFF');
-          logger.debug('<<<< -. temp > (setPoint) turning heater OFF');
-        } else {
-          // console.warn('==== -. temp none of on or off');
+          logger.debug('current_room_temp > (FH_Thermostat_SetPoint) turning heater OFF');
         }
       } else {
         items.getItem('FH_Heater_Control').sendCommand('OFF');
+        logger.debug('Outside_Temperature_raw:{} > FH_outside_temperature_enable:{}: {}', Outside_Temperature_raw, FH_outside_temperature_enable_raw);
+        logger.debug('FH_Heater_Control.sendCommand:{}', 'OFF');
       }
-    } else {
-      logger.debug('NOT_____mvm___PAST THE GATE');
     }
   },
 });
